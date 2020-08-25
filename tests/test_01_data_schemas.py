0b621ca1b7e04d8aa08eb1f594a6a50e5586bb7c
Marc Puig <marc.puig@gmail.com>
2020-08-25 11:01:32 +0200

    Add Book data schema

--- tests/test_01_data_schemas.py
+++ tests/test_01_data_schemas.py
@@ -1,5 +1,14 @@
+from typing import Optional
+
 import pytest
-from pydantic import ValidationError
+from pydantic import BaseModel, constr, confloat, ValidationError
+
+
+class Book(BaseModel):
+    title: str
+    author: constr(min_length=1, regex="[A-Za-z ,.'-]+$")
+    price: confloat(gt=0)
+    year: Optional[int]
 
 
 def test_create_book_with_valid_schema_successfully() -> None:
