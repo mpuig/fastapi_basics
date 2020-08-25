6249be0a32e075f6c848e71e53c1c1bcf18e198b
Marc Puig <marc.puig@gmail.com>
2020-08-25 16:06:26 +0200

    Add test to check the get-by-id API endpoint

--- tests/test_03_api_with_repository.py
+++ tests/test_03_api_with_repository.py
@@ -1,4 +1,5 @@
 from typing import List
+from uuid import UUID
 
 import pytest
 from fastapi import Depends, FastAPI
@@ -18,6 +19,14 @@ def app():
     ) -> List[Book]:
         books = books_repo.list()
         return books
+
+    @app.get("/{book_id}/", name="books:get-book-by-id")
+    def get_book_by_id(
+        book_id: UUID,
+        books_repo: InMemoryBookRepository = Depends(InMemoryBookRepository)
+    ) -> Book:
+        book = books_repo.get(book_id=book_id)
+        return book
 
     @app.post("/", name="books:create-book", status_code=201)
     def create_new_book(
@@ -60,3 +69,18 @@ def test_invalid_create_book_raises_error(app, client) -> None:
     empty_payload = {}
     response_new_book = client.post(url_create_book, json=empty_payload)
     assert response_new_book.status_code == 422
+
+
+def test_get_book_by_id_successfully(app, client, a_book, another_book) -> None:
+    url_create_book = app.url_path_for("books:create-book")
+
+    response_new_book_1 = client.post(url_create_book, json={'book': a_book.dict()})
+    assert response_new_book_1.status_code == 201
+    book1_id = response_new_book_1.json()['id']
+
+    response_new_book_2 = client.post(url_create_book, json={'book': another_book.dict()})
+    assert response_new_book_2.status_code == 201
+
+    get_book_url = app.url_path_for("books:get-book-by-id", book_id=book1_id)
+    response_get_book = client.get(get_book_url)
+    assert a_book == Book(**response_get_book.json())
