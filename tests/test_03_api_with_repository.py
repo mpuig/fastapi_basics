18cb09ece915ea1dddbb394730a7dd040c9c670c
Marc Puig <marc.puig@gmail.com>
2020-08-25 16:57:43 +0200

    Refactor app and client to conftest.py

--- tests/test_03_api_with_repository.py
+++ tests/test_03_api_with_repository.py
@@ -1,47 +1,4 @@
-from typing import List
-from uuid import UUID
-
-import pytest
-from fastapi import Depends, FastAPI
-from fastapi.testclient import TestClient
-
-from tests.repositories import InMemoryBookRepository
 from tests.schemas import Book
-
-
-@pytest.fixture
-def app():
-    app = FastAPI()
-
-    @app.get("/", name="books:get-all-books")
-    def get_all_books(
-        books_repo: InMemoryBookRepository = Depends(InMemoryBookRepository)
-    ) -> List[Book]:
-        books = books_repo.list()
-        return books
-
-    @app.get("/{book_id}/", name="books:get-book-by-id")
-    def get_book_by_id(
-        book_id: UUID,
-        books_repo: InMemoryBookRepository = Depends(InMemoryBookRepository)
-    ) -> Book:
-        book = books_repo.get(book_id=book_id)
-        return book
-
-    @app.post("/", name="books:create-book", status_code=201)
-    def create_new_book(
-        book: Book,
-        books_repo: InMemoryBookRepository = Depends(InMemoryBookRepository)
-    ) -> Book:
-        book_db = books_repo.add(book)
-        return book_db
-
-    return app
-
-
-@pytest.fixture
-def client(app):
-    return TestClient(app)
 
 
 def test_get_all_books_successfully(app, client) -> None:
