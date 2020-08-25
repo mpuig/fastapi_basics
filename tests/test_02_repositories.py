26f615914b109a335be6416970b66868a0632883
Marc Puig <marc.puig@gmail.com>
2020-08-25 12:18:14 +0200

    Add some extra tests to check the get function

--- tests/test_02_repositories.py
+++ tests/test_02_repositories.py
@@ -64,6 +64,11 @@ def a_book() -> Book:
     return Book(title="A nice title", author="John Smith")
 
 
+@pytest.fixture()
+def another_book() -> Book:
+    return Book(title="Another nice title", author="Jane Boo")
+
+
 def test_add_valid_data_to_in_memory_repository_successfully(a_book):
     repo = InMemoryBookRepository()
     repo.add(a_book)
@@ -82,3 +87,26 @@ def test_add_invalid_data_to_n_memory_repository_raises_exception():
 
     items = repo.list()
     assert len(items) == 0
+
+
+def test_get_book_by_id_from_in_memory_repository_successfully(a_book, another_book):
+    repo = InMemoryBookRepository()
+    book1 = repo.add(a_book)
+    book2 = repo.add(another_book)
+
+    response1 = repo.get(book_id=book1.id)
+    assert response1 == book1
+    assert response1.title == a_book.title
+
+    response2 = repo.get(book_id=book2.id)
+    assert response2 == book2
+    assert response2.title == another_book.title
+
+
+def test_get_book_by_id_from_in_memo_repository_with_invalid_id_returns_none(a_book):
+    repo = InMemoryBookRepository()
+    repo.add(a_book)
+
+    invalid_id = UUID('00000000-0000-0000-0000-000000000000')
+    response1 = repo.get(book_id=invalid_id)
+    assert response1 is None
