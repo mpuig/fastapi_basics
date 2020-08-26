13400640cd7019b3d58a8260b2de90d32cc3e14d
Marc Puig <marc.puig@gmail.com>
2020-08-26 10:11:52 +0200

    Tests to read environment variables

--- tests/test_06_settings.py
+++ tests/test_06_settings.py
@@ -30,12 +30,26 @@ class ConfigFileNotFoundException(Exception):
     """Required settings file can't be found."""
 
 
+class MandatoryEnvironmentVariableNotDefinedException(Exception):
+    """Required environment variable not provided."""
+
+
 def load_settings_from_file(settings_file: str) -> Settings:
     if not os.path.exists(settings_file):
         raise ConfigFileNotFoundException(
             f"The settings file {settings_file} could not be found."
         )
     return Settings(_env_file=settings_file)
+
+
+def get_mandatory_environment_variable(environment_variable_name: str) -> str:
+    """None is not considered a valid value."""
+    value = os.getenv(environment_variable_name)
+    if value is None:
+        raise MandatoryEnvironmentVariableNotDefinedException(
+            f"The {environment_variable_name} environment variable is mandatory."
+        )
+    return value
 
 
 def test_load_settings_from_file_successfully(tmp_settings_file: str):
@@ -46,3 +60,23 @@ def test_load_settings_from_file_successfully(tmp_settings_file: str):
 def test_load_settings_from_nonexistent_file_raises_exception():
     with pytest.raises(ConfigFileNotFoundException):
         load_settings_from_file('nonexistent.env')
+
+
+def test_get_missing_mandatory_environment_variable_raises_exception():
+    with pytest.raises(MandatoryEnvironmentVariableNotDefinedException):
+        get_mandatory_environment_variable("A_MISSING_VARIABLE")
+
+
+def test_get_valid_mandatory_environment_variable_successfully():
+    an_environment_var = "A_DUMMY_VAR"
+    assert os.environ.get(an_environment_var) is None  # Checks the pre-test state is safe.
+
+    an_override_value = "value"
+    os.environ[an_environment_var] = an_override_value
+    try:
+        assert (
+            get_mandatory_environment_variable(an_environment_var)
+            == an_override_value
+        )
+    finally:
+        del os.environ[an_environment_var]
